a1647831fde8cbfdcef8cad9bd5ea7ae93303891
Diseyi <filomiinaa@gmail.com>
2021-11-29 12:56:20 +0100

    fixed responsibility on toptalent page

--- groomhive/src/components/header/Header.js
+++ groomhive/src/components/header/Header.js
@@ -1,25 +1,32 @@
 import React from "react";
 import logo from "../../assets/groomHive.svg";
-import './Header.css';
-import { Outlet, Link } from "react-router-dom";
+import "./Header.css";
+import menu from "../../assets/icon-hamburger.svg";
+// import { Outlet, Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="Header" >
+    <div className="Header">
       <div className="Mobile-Header">
         <img src={logo} alt="logo" className="logo" />
-        <button className="Button-Menu" >Menu</button>
+        <button className="Button-Menu">
+          <svg viewBox="0 0 100 60" width="40" height="40">
+            <rect width="100" height="10"></rect>
+            <rect y="23" width="100" height="10"></rect>
+            <rect y="45" width="100" height="10"></rect>
+          </svg>
+        </button>
       </div>
 
       <div className="tablet-Header flex">
-      <img src={logo} alt="logo" className="logo" />
+        <img src={logo} alt="logo" className="logo" />
         <div className="Options-Header Flex-Header">
           <button className="Button-Header">HiveWoman</button>
           <button className="Button-Header">Sponsorship</button>
           <button className="Button-Header">About Us</button>
-          <button className="Button-Menu" >Menu</button>
-         
-        {/* </div>
+          <button className="Button-Menu">Menu</button>
+
+          {/* </div>
 
         <div className="GetStarted-Header Flex-Header"> */}
           {/* <button className="Button-Header skyblue">Apply Now</button>
@@ -27,21 +34,19 @@ const Header = () => {
         </div>
       </div>
 
-
       <div className="Laptop-Header">
-      <img src={logo} alt="logo" className="logo" />
+        <img src={logo} alt="logo" className="logo" />
         <div className="Options-Header Flex-Header">
           <button className="Button-Header">HiveWoman</button>
           <button className="Button-Header">Sponsorship</button>
           <button className="Button-Header">About Us</button>
-        {/* </div>
+          {/* </div>
 
         <div className="GetStarted-Header Flex-Header"> */}
           <button className="Button-Header skyblue">Apply Now</button>
           <button className="Register-Header">Apply Now</button>
         </div>
       </div>
-
     </div>
   );
 };
